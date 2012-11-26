9ea0afdd0d6d7682558ad647ed0966a8d577bb53
Alec G <throwpi@me.com>
2012-11-26 00:39:22 -0700

    Added basic button and EditText call functionality

--- src/com/example/routerCPR/MainActivity.java
+++ src/com/example/routerCPR/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.routerCPR;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private EditText addressText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        addressText = (EditText)findViewById(R.id.address);
     }
 
     @Override
@@ -17,4 +24,10 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public void recoverAction(View v) {
+    	String address = addressText.getText().toString();
+    	Toast.makeText(this, "Recovering " + address, Toast.LENGTH_SHORT).show();
+    }
+    
 }
