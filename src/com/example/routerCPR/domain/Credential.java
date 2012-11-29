49f26ad9ae6ffa357c5eaf670da0fb1b18a2595f
Alec G <throwpi@me.com>
2012-11-29 02:13:20 -0700

    Fixed included ActiveAndroid library and changed classpath to include correct jar. Also modified Credential to use ActiveRecord.

--- src/com/example/routerCPR/domain/Credential.java
+++ src/com/example/routerCPR/domain/Credential.java
@@ -1,8 +1,14 @@
 package com.example.routerCPR.domain;
 
-public class Credential {
-	
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+@Table(name = "Credentials")
+public class Credential extends Model {
+	@Column(name = "user")
 	private final String user;
+	@Column(name = "password")
 	private final String password;
 	
 	public Credential(String user, String password) {
