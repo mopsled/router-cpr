8ea12b586e77535d9e35571186c6f8000995dd94
Alec G <throwpi@me.com>
2012-11-29 01:05:24 -0700

    Moved TryPasswordTask to separate BruteForceTask class

---
+++ src/com/example/routerCPR/BruteForceTask.java
@@ -0,0 +1,97 @@
+package com.example.routerCPR;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.util.List;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.routerCPR.domain.Credential;
+
+public class BruteForceTask extends AsyncTask<Void, Double, Credential> {
+	
+	public enum Error {
+		NONE,
+		INVALID_URL,
+		COULD_NOT_CONNECT
+	};
+	
+	private final String address;
+	private final List<Credential> credentials;
+	private Context context;
+	private Error error;
+	
+	public BruteForceTask(String address, List<Credential> credentials, Context context) {
+		this.address = cleanup(address);
+		this.credentials = credentials;
+		this.context = context;
+	}
+
+	@Override
+	protected Credential doInBackground(Void... params) {
+		error = Error.NONE;
+		
+		URL url;
+		try {
+			url = new URL(address);
+		} catch (MalformedURLException e) {
+			error = Error.INVALID_URL;
+			return null;
+		}
+		
+		for (final Credential credential : credentials) {
+			Authenticator.setDefault(new Authenticator() {
+				@Override
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(credential.getUser(), credential.getPassword().toCharArray());
+				}
+			});
+			
+			HttpURLConnection connection;
+			try {
+				connection = (HttpURLConnection)url.openConnection();
+			} catch (IOException e) {
+				error = Error.COULD_NOT_CONNECT;
+				return null;
+			}
+			
+			int responseCode;
+			try {
+				responseCode = connection.getResponseCode();
+			} catch (IOException e) {
+				error = Error.COULD_NOT_CONNECT;
+			}
+		}
+		
+		return new Credential("hatfullofhallow", "hunter7");
+	}
+	
+	@Override
+	protected void onPostExecute(Credential result) {
+		String status = "User: " + result.getUser() + ", Password: " + result.getPassword();
+		Toast.makeText(context, status, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	protected void onProgressUpdate(Double... values) {
+		// Update UI with progress
+	}
+	
+	public String cleanup(String url) {
+		
+		String cleanUrl = url.trim();
+		
+		if (!cleanUrl.contains("://")) {
+			cleanUrl = "http://" + cleanUrl;
+		}
+		
+		return cleanUrl;
+	}
+}
--- src/com/example/routerCPR/MainActivity.java
+++ src/com/example/routerCPR/MainActivity.java
@@ -23,12 +23,6 @@ import com.example.routerCPR.domain.Credential;
 public class MainActivity extends Activity {
 	
 	private EditText addressText;
-	
-	private enum Error {
-		NONE,
-		INVALID_URL,
-		COULD_NOT_CONNECT
-	};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,80 +41,7 @@ public class MainActivity extends Activity {
     public void recoverAction(View v) {
     	String address = addressText.getText().toString();
     	Toast.makeText(this, "Recovering " + address, Toast.LENGTH_SHORT).show();
-    	new TryPasswordTask("10.0.3.14", new ArrayList<Credential>()).execute();
-    }
-    
-    private class TryPasswordTask extends AsyncTask<Void, Double, Credential> {
-    	
-    	private final String address;
-    	private final List<Credential> credentials;
-    	private Error error = Error.NONE;
-    	
-    	public TryPasswordTask(String address, List<Credential> credentials) {
-    		this.address = cleanup(address);
-    		this.credentials = credentials;
-    	}
-
-		@Override
-		protected Credential doInBackground(Void... params) {
-			
-			URL url;
-			try {
-				url = new URL(address);
-			} catch (MalformedURLException e) {
-				error = Error.INVALID_URL;
-				return null;
-			}
-			
-			for (final Credential credential : credentials) {
-				Authenticator.setDefault(new Authenticator() {
-					@Override
-					protected PasswordAuthentication getPasswordAuthentication() {
-						return new PasswordAuthentication(credential.getUser(), credential.getPassword().toCharArray());
-					}
-				});
-				
-				HttpURLConnection connection;
-				try {
-					connection = (HttpURLConnection)url.openConnection();
-				} catch (IOException e) {
-					error = Error.COULD_NOT_CONNECT;
-					return null;
-				}
-				
-				int responseCode;
-				try {
-					responseCode = connection.getResponseCode();
-				} catch (IOException e) {
-					error = Error.COULD_NOT_CONNECT;
-				}
-			}
-			
-			return new Credential("hatfullofhallow", "hunter7");
-		}
-		
-		@Override
-		protected void onPostExecute(Credential result) {
-			String status = "User: " + result.getUser() + ", Password: " + result.getPassword();
-			Toast.makeText(MainActivity.this, status, Toast.LENGTH_SHORT).show();
-		}
-		
-		@Override
-		protected void onProgressUpdate(Double... values) {
-			Log.d(MainActivity.this.getClass().getName(), Double.toString(values[0]));
-		}
-		
-		public String cleanup(String url) {
-			
-			String cleanUrl = url.trim();
-			
-			if (!cleanUrl.contains("://")) {
-				cleanUrl = "http://" + cleanUrl;
-			}
-			
-			return cleanUrl;
-		}
-    	
+    	new BruteForceTask("10.0.3.14", new ArrayList<Credential>(), this).execute();
     }
     
 }
