7eae0d3823261525192dd9e7b7ac12e23cc17a4a
Alec G <throwpi@me.com>
2012-11-29 01:26:24 -0700

    Modified CredentialHelper to only deal with users and passwords.

--- src/com/example/routerCPR/CredentialHelper.java
+++ src/com/example/routerCPR/CredentialHelper.java
@@ -1,12 +1,15 @@
 package com.example.routerCPR;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.routerCPR.domain.Credential;
+
 public class CredentialHelper extends SQLiteOpenHelper {
 	
-	private static final String DATABASE_NAME = "credential.db";
+	private static final String DATABASE_NAME = "routercpr.db";
 	private static final int SCHEMA_VERSION = 1;
 	
 	private static final String CREATE_TABLE = "CREATE TABLE credentials (id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, password TEXT)";
@@ -23,6 +26,14 @@ public class CredentialHelper extends SQLiteOpenHelper {
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// Nothing to upgrade yet
+	}
+	
+	public long insertCredential(Credential credential) {
+		ContentValues contentValues = new ContentValues();
+		contentValues.put("user", credential.getUser());
+		contentValues.put("password", credential.getPassword());
+		
+		return getWritableDatabase().insert("credentials", "user", contentValues);
 	}
 
 }
